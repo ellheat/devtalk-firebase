af7e5176592739433f0ea07b408ba51b728166de
Patryk Poleszczuk <patryk.poleszczuk@gmail.com>
2018-02-20 18:32:15 +0100

    Add MessageInput component and translations

---
+++ app/routes/chat/messages/message/message.component.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+
+
+export class Message extends PureComponent {
+  static propTypes = {};
+
+  render = () => (
+    <div className="message">
+
+    </div>
+  );
+}
---
+++ app/routes/chat/messages/messages.styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex: 1;
+  width: 100%;
+`;
