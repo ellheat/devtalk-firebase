9f166d4595bcc185d1da794bd5e41629b3985856
Rafał Gruszecki <rgruszecki@apptension.com>
2018-03-04 20:29:17 +0100

    Add room on putting message

--- app/modules/chat/chat.sagas.js
+++ app/modules/chat/chat.sagas.js
@@ -3,10 +3,11 @@ import reportError from 'report-error';
 import { reset as resetForm } from 'redux-form/immutable';
 
 import { ChatTypes, ChatActions } from './chat.redux';
-import { messagesRef, timestamp } from '../../services/firebase';
+import { messagesRef, roomsRef, timestamp } from '../../services/firebase';
 
 export function* sendChatMessage({ roomId, author, message }) {
   try {
+    yield roomsRef.child(roomId).set(true);
     yield messagesRef.child(roomId).push({ author, message, timestamp });
     yield put(resetForm('messageForm'));
   } catch (e) {
